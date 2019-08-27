2b8af6ed0a02f2235fa7a4ad787c315a6c0c1de0
Teresa Hsieh <teresahsieh850506@gmail.com>
2019-08-27 14:11:57 +0800

    setup the environment

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js"
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
+};
