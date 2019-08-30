735939c298e7bc0004b373d622732c483a8ec6e0
Teresa Hsieh <teresahsieh850506@gmail.com>
2019-08-30 11:26:38 +0800

    import firebase/firestore

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+// Initialize Firebase
+var firebaseConfig = {
+  apiKey: "AIzaSyAu5M8SOdRdZjRp6HwRB9LqdkdUyNTvNlg",
+  authDomain: "healthy-match.firebaseapp.com",
+  databaseURL: "https://healthy-match.firebaseio.com",
+  projectId: "healthy-match",
+  storageBucket: "healthy-match.appspot.com",
+  messagingSenderId: "560759415450",
+  appId: "1:560759415450:web:9fc855be631767dc"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
